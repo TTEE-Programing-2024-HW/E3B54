29ea3342111b5d3fc9aff6c17ec058bb9ca24fed
ZhenAn <u11002254@o365.ttu.edu.tw>
2024-04-30 22:29:07 +0800

    完成作業2和3題的要求
    讓螢幕出現主選單，若使用者輸入a或A，則讓其輸入a~n的英文字母，並使其產生直角三角形，後回到主選單

--- 程設HW.c
+++ 程設HW.c
@@ -1,9 +1,10 @@
-#include<stdio.h>
-#include<stdlib.h>
-int main(void)
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+
+int main(void) 
 {
-	/*個人風格畫面*/ 
-	printf("	              .:--.                   :--.        .  .\n");   
+    printf("	              .:--.                   :--.        .  .\n");
     printf("             :-.  :-:              .-++++*=    . .. .. .  \n");
     printf("             -.*+-. .::.--==++++=--*+++****-   . ....  .  \n");
     printf("            .-:****=.       ----.++++*****++   .  .... .   \n");
@@ -34,38 +35,88 @@ int main(void)
     printf("       .::-+-.. --:-::..                :=++*+:::::        \n");
     printf("        ::::-:--:.   .:-----------::::::+++=: ::::         \n");
     printf("         .:::                                 ..           \n");
-    
-	system("pause");
+
+    system("pause");
     system("CLS");
-    
-    int password,error=0;
-    
-	while(error<3)
-	{ 
-	printf("請輸入密碼:");
-    scanf("%d",&password);
-    
-    if(password==2024)
-        {
-    	printf("歡迎使用\n");
-    	
-    	system("pause");
-    	system("CLS");
-	    }
-	else
-	    {
-		error++;
-		printf("密碼錯誤請重新輸入\n");
-		
-		if(error==3)
+
+    int password, error = 0;
+
+    while (error < 3) 
+	{
+        printf("請輸入密碼:");
+        scanf("%d", &password);
+
+        if (password == 2024) 
 		{
-			printf("你已連續錯三次，程式即將關閉\n");
-			
-			
-			break;
-		}
-	    }   
-	}
+            printf("歡迎使用!\n");
+            break;
+        } 
+		else 
+		{
+            error++;
+            printf("密碼錯誤!請重新輸入!\n");
+
+            if (error == 3) 
+			{
+                printf("錯誤已達三次，程式即將關閉!\n");
+                break;
+            }
+        }
+    }
+    printf("密碼值：%d\n", password);
+
+    system("pause");
+    system("CLS");
+
+    while (1) 
+	{
+        printf("--------------------------\n");
+        printf("|  a. 畫出直角三角形     |\n");
+        printf("|  b. 顯示乘法表         |\n");
+        printf("|  c. 結束               |\n");
+        printf("--------------------------\n");
+
+        char choice, triangle;
+        int i,j;
+        printf("請選擇使用項目:");
+        fflush(stdin);
+        scanf(" %c", &choice);
+
+        if (choice == 'a' || choice == 'A') 
+		{
+            system("CLS");
+
+            printf("請輸入'a'到'n'之間的一個字元：");
+            fflush(stdin);
+            scanf(" %c", &triangle);
+
+            if (triangle<'a' || triangle>'n') 
+			{
+                printf("請輸入'a'到'n'之間的一個字元!\n");
+                continue;
+            } 
+			else 
+			{
+                printf("以下是以'%c'為底的直角三角形：\n", triangle);
+                int rows = triangle - 'a' + 1;
+                for (i = 0;i<rows;i++) 
+				{
+                    for ( j = 0; j < rows - i - 1; j++) 
+					{
+                        printf(" ");
+                    }
+                    char ch = 'a';
+                    for ( j = 0; j <= i; j++) 
+					{
+                        printf("%c", ch++);
+                    }
+                    printf("\n");
+            }
+            system("pause");
+            system("CLS");
+            } 
+        }
+    } 
 	return 0;
 }
-    
+
