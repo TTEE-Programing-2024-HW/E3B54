3a291ddb8831978d47d59b7018974fc278583fe7
ZhenAn <u11002254@o365.ttu.edu.tw>
2024-05-20 11:43:35 +0800

    完成第二題要求
    讓主選單印出來

--- hw3.c
+++ hw3.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <conio.h>
+
+#define ROWS 9
+#define COLS 9
 
 int main(void) 
 {
@@ -65,6 +67,41 @@ int main(void)
         }
     }
     system("pause");
-    return 0;
+    system("CLS");
+
+char seats[ROWS][COLS];
+int prebooked[10][2]=
+{
+    {0, 1}, {1, 4}, {2, 7}, {3, 8}, {4, 2}, 
+    {5, 5}, {6, 6}, {7, 3}, {8, 0}, {8, 7}
+};
+int i,j;
+for (i=0;i<ROWS;++i) 
+{
+        for (j=0;j<COLS;++j) 
+		{
+            seats[i][j] = '-';
+        }
+}
+for (i=0;i<10;++i) 
+{
+    seats[prebooked[i][0]][prebooked[i][1]] = '*';
+}
+char choice;
+   
+while(choice!='q');
+{
+    printf("----------[BookingSystem]----------\n");
+    printf("|  a. Available seats             |\n");
+    printf("|  b. Arrange for you             |\n");
+    printf("|  c. Choose by yourself          |\n");
+    printf("|  d. Exit                        |\n");
+    printf("------------------------------------\n");
+    
+    printf("Enter your choice: ");
+    scanf("%c",&choice);
+    
 }
 
+return 0;
+}
