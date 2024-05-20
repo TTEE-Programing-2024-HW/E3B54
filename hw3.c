0f3f5af7b874c8fc43f24c6b04b183d504145f87
ZhenAn <u11002254@o365.ttu.edu.tw>
2024-05-20 17:36:01 +0800

    完成第四題要求
    讓使用者輸入b時，出現推薦的座位，並且讓其選擇是否要採用推薦

--- hw3.c
+++ hw3.c
@@ -103,7 +103,9 @@ while (count<10)
 }
 
 char choice;
-   
+int suggested_seats[4][2];
+int found = 0;
+  
 do
 {
 	system("CLS");
@@ -137,6 +139,148 @@ do
         getchar();
     }
     
+    if (choice=='b') 
+	{
+        int num_seats;
+        printf("How many seats do you need? (1-4): ");
+        scanf("%d", &num_seats);
+        if (num_seats<1||num_seats>4) 
+		{
+            printf("Invalid number of seats.\n");
+            continue;
+        }
+        
+        found=0; 
+        
+        do{
+            if(num_seats <= 3) 
+		    {
+			int row,col;
+            for(row = 0; row<ROWS&&!found;++row) 
+			{
+                for(col=0;col<=COLS-num_seats&&!found;++col) 
+				{
+                    int available=1;
+                    int k;
+                    for(k=0; k<num_seats;++k) 
+					{
+                        if(seats[row][col + k]!='-') 
+						{
+                            available=0;
+                            break;
+                        }
+                    }
+                        if(available) 
+						{	
+                            for(k=0;k<num_seats;++k) 
+							{
+                                suggested_seats[k][0]=row;
+                                suggested_seats[k][1]=col + k;
+                            }
+                            found=1;
+                        }
+                }
+            }
+        } 
+		else {
+			    int row,col;
+                for(row = 0; row<ROWS&&!found;++row) 
+				{
+                    for(col=0;col<=COLS-4 && !found;++col) 
+					{
+                        int available=1;
+                        int k;
+                        for(k=0;k<4;++k) 
+						{
+                            if(seats[row][col + k] != '-') 
+							{
+                                available = 0;
+                                break;
+                            }
+                        }
+                        if(available) 
+						{
+                            for(k = 0; k < 4; ++k) 
+							{
+                                suggested_seats[k][0] = row;
+                                suggested_seats[k][1] = col + k;
+                            }
+                            found = 1;
+                        }
+                    }
+                }
+                for (row=0;row<ROWS - 1 && !found; ++row) 
+				{
+                    for (col=0; col<=COLS - 2 && !found; ++col) 
+					{
+                        int available = 1;
+                        int k;
+                        for (k = 0; k < 2; ++k) 
+						{
+                            if (seats[row][col + k] != '-' || seats[row + 1][col + k] != '-') 
+							{
+                                available = 0;
+                                break;
+                            }
+                        }
+                        if (available) 
+						{
+                            for (k = 0; k < 2; ++k) 
+							{
+                                suggested_seats[k][0] = row;
+                                suggested_seats[k][1] = col + k;
+                                suggested_seats[k + 2][0] = row + 1;
+                                suggested_seats[k + 2][1] = col + k;
+                            }
+                            found = 1;
+                        }
+                    }
+                }
+            }
+
+            if (found) 
+			{
+                for (i = 0; i < num_seats; ++i) 
+				{
+                    seats[suggested_seats[i][0]][suggested_seats[i][1]] = '@';
+                }
+                printf("//987654321\n");
+                int x=9;
+                for (i = 0; i < ROWS; ++i) 
+				{
+                    printf("%d ",x--);
+                    for (j = 0; j < COLS; ++j) 
+					{
+                        printf("%c", seats[i][j]);
+                    }
+                    printf("\n");
+                }
+
+                char satisfied;
+                printf("Are you satisfied with the arrangement? (y/n): ");
+                scanf(" %c", &satisfied);
+                if (satisfied == 'y' || satisfied == 'Y') 
+				{
+                    for (i = 0; i < num_seats; ++i) 
+					{
+                        seats[suggested_seats[i][0]][suggested_seats[i][1]] = '*';
+                    }
+                } 
+				else 
+				{
+                    for (i = 0; i < num_seats; ++i) 
+					{
+                        seats[suggested_seats[i][0]][suggested_seats[i][1]] = '-';
+                    }
+                    break;
+                }
+            } 
+			else 
+			{
+                printf("No suitable arrangement found.\n");
+            }
+	}while (!found);
+}
 }while(choice!='q');
 
 return 0;
