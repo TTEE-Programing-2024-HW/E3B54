b0647482f425c124a97fdf7cc6e4a9fee6ccfd24
ZhenAn <u11002254@o365.ttu.edu.tw>
2024-05-27 22:39:01 +0800

    完成個人風格畫面
    設定好每個步驟的函數，並且完成了個人風格畫面

---
+++ hw4.c
@@ -0,0 +1,102 @@
+#include <stdio.h>
+#include<stdlib.h>
+#include <string.h>
+
+#define MAX_STUDENTS 10
+#define password 2024
+
+typedef struct 
+{
+    char name[50];
+    int id;
+    int math;
+    int physics;
+    int english;
+} Student;
+
+Student students[MAX_STUDENTS];
+int numStudents = 0;
+
+void Welcome();
+int Password();
+void Menu();
+void Enter();
+void Display();
+void Search();
+void Ranking();
+void exit();
+
+int main() 
+{
+    Welcome();
+
+    if (!Password()) 
+	{
+        printf("錯誤已達三次，請離開!\n");
+        return 1;
+    }
+
+    while (1) 
+	{
+        system("CLS");
+        Menu();
+        char choice;
+        scanf(" %c", &choice);
+
+        switch (choice) {
+            case 'a':
+                Enter();
+                break;
+            case 'b':
+                Display();
+                break;
+            case 'c':
+                Search();
+                break;
+            case 'd':
+                Ranking();
+                break;
+            case 'e':
+                Exit();
+                return 0;
+            default:
+                printf("無效指令，請重新輸入!\n");
+        }
+    }
+}
+
+void Welcome() 
+{
+    system("CLS");
+    printf("	              .:--.                   :--.        .  .\n");
+    printf("             :-.  :-:              .-++++*=    . .. .. .  \n");
+    printf("             -.*+-. .::.--==++++=--*+++****-   . ....  .  \n");
+    printf("            .-:****=.       ----.++++*****++   .  .... .   \n");
+    printf("            -:-**=-:.            =++++*****+   .  .   .     \n");
+    printf("            =.-+.                .++++++**+*        .      \n");
+    printf("            =.:.                  :++++++*+#:              \n");
+    printf("            +.  .-+*##*-         :+%%%#**++*+              \n");
+    printf("           .+ ..:    .=##       =#=::++***++#::=---=:      \n");
+    printf("          .+..:-::                    .-+===+*=.:- :+      \n");
+    printf("          =- -=-:.         .+:         .::-:.*-:--::=.     \n");
+    printf("          *  . .:.    .::... ..::::    .. .:::+     +.     \n");
+    printf("          *  .=-                        .:-. .*     +      \n");
+    printf("          +.                              .: .+ :=++=      \n");
+    printf("          :+   .                             +.-+++*:      \n");
+    printf("           :+=+=++=.                       :=. +++++       \n");
+    printf("          .-+::-+:-+=                   :=**   :=+*.       \n");
+    printf("         +--+=+*===-=*+=-:..      .::-+***+.     +:        \n");
+    printf("        -=::++==+::::=#****#*****#*****+-.      =:         \n");
+    printf("        -=:-+::-+-==::+:.=+===+=--::..        :=.          \n");
+    printf("         :=*::-=*=++-:-++------+:            :=            \n");
+    printf("          .#::--*--+-::+==------+.           =.            \n");
+    printf("           +=::+=:-=++:+=:--=:::-          :==---.         \n");
+    printf("          :=*-:=++*+++=*:--==---         .=: . :.+         \n");
+    printf("         +-.-+--*==++-==  ....          :+ .==-..=.        \n");
+    printf("        :+....=+=-=-:-+.                +.  .:=..=         \n");
+    printf("        .+:==. .-===--                  +.     -=          \n");
+    printf("        .:=.     .                      .-=---+:.::.       \n");
+    printf("       .::-+-.. --:-::..                :=++*+:::::        \n");
+    printf("        ::::-:--:.   .:-----------::::::+++=: ::::         \n");
+    printf("         .:::                                 ..           \n");
+}
